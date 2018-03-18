7be99acf1055af0be573f14e76ca043635b849d3
Matthew Troncali <matt@troncali.net>
2018-03-18 14:00:51 -0700

    Revised brew formulae to be installed.

--- scripts/brew.sh
+++ scripts/brew.sh
@@ -17,16 +17,21 @@ printf "\ntroncali/.setup : Homebrew
 # Is Homebrew installed?
 if test ! $(which brew); then
 	printf "Installing Homebrew and commonly used formulae and applications.\n"
-	#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 else
 	printf "Updating Homebrew and commonly used formulae and applications.\n"
-	#brew update
-	#brew upgrade
+	brew update
+	brew upgrade
 fi
+
+## TODO: Create a function to (1) test for existing installations; and
+##		 (2) perform installations that do not exist.
 
 ###############################################################################
 # Update macOS Tools                                                          #
 ###############################################################################
+
+echo "    -> Update macOS Tools."
 
 # Install GNU core utilities (macOS versions are outdated) and other GNU utils.
 #     Note: '$PATH' modifications in .zshrc point to GNU utilities before macOS 
@@ -34,135 +39,183 @@ fi
 # Remove these paths from .zshrc if you prefer to leave macOS default utilities
 # in tact and access GNU utilities with the 'g' prefix. If using a different 
 # shell, be sure to update your '$PATH' accordingly.
-echo "    -> GNU core and related utilities."
-#brew install coreutils
-#brew install moreutils
-#brew install findutils
-#brew install gnu-sed
-#brew install wget --with-iri
-#brew install grep
-#brew install screen
-#brew install rsync
+echo "       :: GNU core and related utilities."
+brew install coreutils
+brew install moreutils
+brew install findutils
+brew install gnu-sed
+brew install wget --with-iri
+brew install grep
+brew install screen
+brew install rsync
 
 # Install Zsh, add to the list of allowed shells, and make it the active shell.
-# TODO: What about ZSH completion? oh-my-zsh?
-echo "    -> Zsh."
-#brew install zsh
-#sudo bash -c 'echo /usr/local/bin/zsh >> /etc/shells'
-#chsh -s /usr/local/bin/zsh
+echo "       :: Zsh."
+brew install zsh
+sudo bash -c 'echo /usr/local/bin/zsh >> /etc/shells'
+chsh -s /usr/local/bin/zsh
 
 # Install Vim
-echo "    -> Vim."
-#brew install vim --with-override-system-vi
+echo "       :: Vim."
+brew install vim --with-override-system-vi
 
 # Install OpenSSH
-echo "    -> OpenSSH."
-#brew install openssh
+echo "       :: OpenSSH."
+brew install openssh
 
 # Install git
-echo "    -> Git."
-#brew install git
+echo "       :: Git and related tools."
+brew install git
+brew cask install sourcetree	# Git GUI.
+brew install git-flow	# High-level repository operations for Vincent Driessen's branching model.
+brew install git-lfs	# Git extention for versioning large files.
+brew install bfg		# Simpler, faster (but less sophisticated) alternative to git-filter-branch.
+#brew install git-extras
+#brew install hub		# Command line wrapper for git.
 
 # Install Quick Look plugins
-echo "    -> Quick Look plugins."
-#brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json quicklook-csv betterzipql qlimagesize suspicious-package
+echo "       :: Quick Look plugins."
+brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json quicklook-csv betterzipql qlimagesize suspicious-package
 
-# CLI control over macOS 'Dark Mode' 
-echo "    -> CLI for macOS 'Dark Mode'."
-#brew install dark-mode
-
+# Install XQuartz
+echo "       :: XQuartz."
+brew cask install xquartz
 
 ###############################################################################
 # Install macOS Applications                                                  #
 ###############################################################################
 
-## TODO: Describe applications being installed;
-##		 Search for other applications to include ('brew cask search')
+echo "    -> Install applications."
 
-#brew cask install xquartz
-#brew cask install java
-#brew cask install google-chrome
-#brew cask install dropbox
+# Install 1Password
+echo "       :: 1Password."  ## TODO: Lastpass?
+brew cask install 1password 1password-cli
+
+# Install Cyberduck
+echo "       :: Cyberduck."
+brew cask install cyberduck
+
+# Install Dash
+echo "       :: Dash."
+brew cask install dash
+
+# Install Dropbox
+echo "       :: Dropbox."
+brew cask install dropbox
+
+# Install FontForge
+echo "       :: FontForge."
+brew cask install fontforge
+
+# Install Chrome and DevTools
+echo "       :: Google Chrome & DevTools."
+brew cask install google-chrome chrome-devtools
+
+# Install Inkscape
+echo "       :: Inkscape."
+brew cask install inkscape
+
+# Install Java
+echo "       :: Java."
+brew cask install java
+
+# Install Microsoft Office
+## TODO: Libre/Open Office?
+echo "       :: Microsoft Office."
+brew cask install microsoft-office
+
+# Install Parallels
+echo "       :: Parallels."
+brew cask install parallels
+
+# Install Real VNC
+## TODO: license; Other VNC server?
+echo "       :: Real VNC 5.3.2."
+brew cask install https://raw.githubusercontent.com/caskroom/homebrew-cask/326d6a92b0c06c9a78c347ded4e1fd7e3f7d78a5/Casks/real-vnc.rb
+
+# Install Spark
+echo "       :: Spark."
+brew cask install spark
+
+# Install Spotify
+echo "       :: Spotify."
+brew cask install spotify
+
+# Install TextMate
+echo "       :: TextMate."
+brew cask install textmate
+
+# Install SparkleShare
+#echo "       :: SparkleShare."
+#brew cask install sparkleshare
 
 ###############################################################################
 # Install Miscellaneous Binaries                                              #
 ###############################################################################
 
-## TODO: Identify binaries from adapted files.
-
-###### mathias
-# Install font tools.
-brew tap bramstein/webfonttools
-brew install sfnt2woff
-brew install sfnt2woff-zopfli
-brew install woff2
-
-# Install some CTF tools; see https://github.com/ctfs/write-ups.
-brew install aircrack-ng
-brew install bfg
-brew install binutils
-brew install binwalk
-brew install cifer
-brew install dex2jar
-brew install dns2tcp
-brew install fcrackzip
-brew install foremost
-brew install hashpump
-brew install hydra
-brew install john
-brew install knock
-brew install netpbm
-brew install nmap
-brew install pngcheck
-brew install socat
-brew install sqlmap
-brew install tcpflow
-brew install tcpreplay
-brew install tcptrace
-brew install ucspi-tcp # `tcpserver` etc.
-brew install xpdf
-brew install xz
-
-# Install other useful binaries.
+echo "    -> Install other useful CLI tools."
 brew install ack
-brew install dark-mode
-#brew install exiv2
-brew install git
-brew install git-lfs
-brew install imagemagick --with-webp
-brew install lua
-brew install lynx
-brew install p7zip
-brew install pigz
-brew install pv
-brew install rename
-brew install rhino
-brew install speedtest_cli
-brew install ssh-copy-id
-brew install testssl
-brew install tree
-brew install vbindiff
-brew install webkit2png
-brew install zopfli
+brew install imagemagick --with-webp	# Bitmap image manipulation tool.
+brew install pv 			# Tool to monitor the progress of data through a pipeline.
+brew install rename 		# Tool to rename files according to modification rules.
+brew install speedtest_cli 	# Command line interface for testing internet bandwith using speedtest.net
+brew install tree 			# Recursive directory listing command producing indented listing of files.
+brew install dark-mode		# CLI for macOS 'Dark Mode'.
+#brew install exiv2 		# Utility to manage image metadata.
+#brew install lua 			# Lua programming language.
+#brew install lynx 			# Web browser for Unix and VMS platforms.
+#brew install p7zip 		# Command line version of 7-Zip.
+#brew install pigz 			# Parallel implementation of gzip.
+#brew install rhino 		# Open-source implementation of JavaScript written in Java.
+#brew install ssh-copy-id 	# Add a public key to a remote machine's authorized_keys file
+#brew install testssl 		# TLS/SSL security testing.
+#brew install vbindiff 		# Displays files in hexadecimal and ASCII, highlighting differences.
+#brew install webkit2png 	# Create screenshots of webpages from the terminal.
+#brew install zopfli 		# Zopfli compression algorithm
+#brew install libxml2		# XML C parser and toolkit.
+#brew install libxslt		# XSLT C library.
+#brew link libxml2 --force
+#brew link libxslt --force
+#brew install pkg-config 	# Tool to help insert compiler options.
+#brew install libffi		# Foreign Function Interface Library.
+#brew install pandoc		# Tool to convert one markup format to another.
+#brew install homebrew/php/php56 --with-gmp	# PHP 5.6
 
+### WEB FONT TOOLS.
+#brew tap bramstein/webfonttools
+#brew install sfnt2woff
+#brew install sfnt2woff-zopfli
+#brew install woff2
 
-#### donnemartin
-brew install heroku-toolbelt
-brew install libxml2
-brew install libxslt
-brew link libxml2 --force
-brew link libxslt --force
-brew install pkg-config libffi
-brew install pandoc
-brew install homebrew/php/php56 --with-gmp
-brew install homebrew/x11/xpdf
-brew install git-flow
-brew install git-extras
-brew install hub
-
+### CAPTURE-THE-FLAG TOOLS: see https://github.com/ctfs/write-ups.
+#brew install aircrack-ng	# Suite of tools to assess WiFi network security.
+#brew install binutils
+#brew install binwalk		# Tool to analyze and extract firmware images.
+#brew install cifer			# Tool to automate classical cipher cracking in C.
+#brew install dex2jar		# Tools to work with android .dex and java .class files.
+#brew install dns2tcp		# Tools to encapsulate TCP session in DNS packets.
+#brew install fcrackzip		# Tool to crack zip passwords.
+#brew install foremost		# Tool to recover files based on headers, footers, and data structures.
+#brew install hashpump		# Tool to exploit the hash length extension attack.
+#brew install hydra			# Network logon cracker.
+#brew install john			# John the Ripper password cracker.
+#brew install knock			# Port-knock client.
+#brew install netpbm		# Toolkit for manipulation of graphic images.
+#brew install nmap			# Network Mapper, utility for network discovery and security auditing.
+#brew install pngcheck		# PNG image integrity verification and statistics.
+#brew install socat			# Bidirectional data transfer between independent data channels.
+#brew install sqlmap		# Penetration testing tool to automate process of detecting adn exploiting SQL injection flaws.
+#brew install tcpflow		# TCIP/IP packet demultiplexer.
+#brew install tcpreplay		# Tools for editing and replaying captured network traffic.
+#brew install tcptrace		# Tool for analysis of TCP dump files.
+#brew install ucspi-tcp		# `tcpserver` etc.
+#brew install xpdf			# PDF viewer and toolkit.
+#brew install xz			# Data compression software.
 
 ###############################################################################
 # Clean Up                                                                    #
 ###############################################################################
 
-#brew cleanup
+echo "    -> Clean up."
+
+brew cleanup
