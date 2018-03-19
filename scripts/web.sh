c6e643d3157ebd5930016c1b7b4d80af1140e166
Matthew Troncali <matt@troncali.net>
2018-03-18 22:17:18 -0700

    Add brew_it(); create npm_it() function based on brew_it(); factor formulae and node packages into arrays.

--- scripts/web.sh
+++ scripts/web.sh
@@ -13,22 +13,94 @@ printf "\ntroncali/.setup : Web Tools
 ===================================================
 Configuring JavaScript development environment.\n"
 
+###############################################################################
+# Brew Functions                                                              #
+###############################################################################
+
 # Is Homebrew installed?
 if test ! $(which brew); then
-	printf "    -> Installing Homebrew.\n"
-	#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	printf "    -> Installing Homebrew and commonly used formulae and applications.\n"
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /dev/null
 else
-	printf "    -> Updating Homebrew.\n"
-	#brew update
-	#brew upgrade
+	printf "    -> Updating Homebrew and commonly used formulae and applications.\n"
+	brew update > /dev/null
+	brew upgrade > /dev/null
 fi
 
-# Install Node and clean up Homebrew.
-echo "    -> Node."
-#brew install node
-#brew cleanup
+# Process each installation array.  See 'brew.sh' for explanations.
+function brew_it() {
+	local commands=${1}
+	local pantry=("${!2}")
+	local options=${3}
+	
+	if [ "${pantry[0]}" == "" ]; then 
+		return
+	fi
+		
+	for formula in "${pantry[@]}" ; do
+	    key=${formula%%:*}
+	    name=${key%% *}
+	    description=${formula#*: }
+		options=${key#* }
 
-# Install Node Packages.
-echo "    -> Grunt."
-#npm install -g grunt-cli
+		if [[ $(brew `[[ $commands == cask* ]] && echo "cask"` ls --versions "$name" 2> /dev/null) ]]; then
+			continue
+		else
+			[[ ! -z "$description" ]] && echo "       :: $description."
+			#brew $commands $name $options > /dev/null
+		fi		
+	done
+}
+
+###############################################################################
+# Web Tools and Applications                                                  #
+###############################################################################
+
+WEB_APPS=(	"node 							: Node"
+#			"homebrew/php/php56 --with-gmp 	: PHP 5.6"
+#			"heroku-toolbelt 				: Heroku Toolbelt"
+			"chrome-devtools 				: Google DevTools"
+)
+	
+echo "    -> Install useful web development tools."	
+brew_it "install" WEB_APPS[@]
+#heroku update > /dev/null
+
+###############################################################################
+# Node Packages				                                                  #
+###############################################################################
+
+# Process each installation array.
+function npm_it() {
+	local pkgs=("${!1}")
+	
+	if [ "${pkgs[0]}" == "" ]; then 
+		return
+	fi
+		
+	for pkg in "${pkgs[@]}" ; do
+	    name=${pkg%%:*}
+	    description=${pkg#*: }
+
+		if [[ $(npm list --depth 1 -g "$name" > /dev/null 2>&1) ]]; then
+			continue
+		else
+			[[ ! -z "$description" ]] && echo "       :: $description."
+			#npm install -g $name > /dev/null
+		fi		
+	done
+}
+
 # TODO: Add other node packages.
+NPM_PKGS=(	"grunt-cli : Grunt"
+)
+
+echo "    -> Install node packages."	
+npm_it NPM_PKGS[@]
+
+###############################################################################
+# Clean Up					                                                  #
+###############################################################################
+
+echo "    -> Clean up."
+brew cleanup
