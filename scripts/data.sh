d832dd83f1980bdc031d46061146bc285f8c33e2
Matthew Troncali <matt@troncali.net>
2018-03-18 22:16:27 -0700

    Add brew_it(); factor formulae into arrays.

--- scripts/data.sh
+++ scripts/data.sh
@@ -13,24 +13,67 @@ printf "\ntroncali/.setup : Databases
 ===================================================
 Configuring databases and related tools.\n"
 
+###############################################################################
+# Brew Functions                                                              #
+###############################################################################
+
 # Is Homebrew installed?
 if test ! $(which brew); then
-	printf "    -> Installing Homebrew.\n"
-	#ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+	printf "    -> Installing Homebrew and commonly used formulae and applications.\n"
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" > /dev/null
 else
-	printf "    -> Updating Homebrew.\n"
-	#brew update
-	#brew upgrade
+	printf "    -> Updating Homebrew and commonly used formulae and applications.\n"
+	brew update > /dev/null
+	brew upgrade > /dev/null
 fi
 
-# Install databases
-echo "    -> MySQL."
-#brew install mysql
-echo "    -> PostgreSQL."
-#brew install postgresql
+# Process each installation array.  See 'brew.sh' for explanations.
+function brew_it() {
+	local commands=${1}
+	local pantry=("${!2}")
+	local options=${3}
+	
+	if [ "${pantry[0]}" == "" ]; then 
+		return
+	fi
+		
+	for formula in "${pantry[@]}" ; do
+	    key=${formula%%:*}
+	    name=${key%% *}
+	    description=${formula#*: }
+		options=${key#* }
 
-# Instal MySQL Workbench
-#brew cask install mysqlworkbench
+		if [[ $(brew `[[ $commands == cask* ]] && echo "cask"` ls --versions "$name" 2> /dev/null) ]]; then
+			continue
+		else
+			[[ ! -z "$description" ]] && echo "       :: $description."
+			#brew $commands $name $options > /dev/null
+		fi		
+	done
+}
 
-# Clean Up
-#brew cleanup
+###############################################################################
+# Data Stores & Applications                                                  #
+###############################################################################
+
+DATABASES=(	"mysql 				: MySQL"
+			"postgresql 		: PostgresSQL"
+)
+	
+DATA_APPS=(	"sequel-pro 		: Sequel Pro"
+#			"mysqlworkbench 	: MySQL Workbench"
+			"valentina-studio 	: Valentina Studio"
+)
+	
+echo "    -> Install database systems."	
+brew_it "install" DATABASES[@]
+
+echo "    -> Install data applications."	
+brew_it "cask install" DATA_APPS[@]
+
+###############################################################################
+# Clean Up					                                                  #
+###############################################################################
+
+echo "    -> Clean up."
+brew cleanup
