d128e48e09abfb57e7f44c97ee82074416f99dd2
Matthew Troncali <matt@troncali.net>
2018-04-29 14:14:31 -0700

    Set file location variables; clean up paths for Viewer build.

--- scripts/vnc.sh
+++ scripts/vnc.sh
@@ -3,20 +3,101 @@
 #####################################################################
 # CREDITS & LICENSE #################################################
 #####################################################################
+# Adapted from:
+# - https://github.com/TigerVNC/tigervnc/blob/master/BUILDING.txt
+#
 # Distributed under the MIT license.
 #####################################################################
 
+echo "    -> Building and installing TigerVNC Viewer and Server."
 
 ###############################################################################
-# Build TigerVNC                                                              #
+# Set Locations                                                               #
 ###############################################################################
 
-echo "    -> Building and installing TigerVNC."
-#ASSETS=~/.setup-assets
+SETUP_ASSETS=~/.setup-assets
 
-#git clone git://github.com/TigerVNC/tigervnc.git "$ASSETS"/TigerVNC/source
-#cd "$ASSETS"/TigerVNC
-#cmake -G "Unix Makefiles" source
-#make
+TIGER_URL=github.com/TigerVNC/tigervnc.git
+TIGER_DIR="${SETUP_ASSETS}/TigerVNC/source"
 
+ORIG_PATH="$PATH"
+X11_LOC="opt/X11"
+BREWED_LOC="/usr/local/opt" # Assumes dependencies are installed by homebrew.
+GETTEXT_LOC="$BREWED_LOC/gettext"
+LIBICONV_LOC="$BREWED_LOC/libiconv"
+JPEG_LOC="$BREWED_LOC/jpeg-turbo"
+LIBTOOL_LOC="$BREWED_LOC/libtool"
+GNUTLS_LOC="$BREWED_LOC/gnutls"
+
+###############################################################################
+# Source TigerVNC                                                             #
+###############################################################################
+
+if [ ! -d $TIGER_DIR ]; then
+    git clone git://"$TIGER_URL" "$TIGER_DIR" --quiet
+else
+    cd "$TIGER_DIR"
+    git pull --quiet
+fi
+
+cd "$SETUP_ASSETS"/TigerVNC
+
+###############################################################################
+# TigerVNC Viewer                                                             #
+###############################################################################
+
+PATH="$LIBICONV_LOC/bin:$JPEG_LOC/bin:$LIBTOOL_LOC/bin:$ORIG_PATH"
+PATH="$X11_LOC:$GETTEXT_LOC/bin:$PATH"
+
+cmake -E env cmake -G "Unix Makefiles" \
+        -DGETTEXT_INCLUDE_DIR=$GETTEXT_LOC/include \
+        -DLIBINTL_LIBRARY=$GETTEXT_LOC/lib/libintl.dylib \
+        -DICONV_INCLUDE_DIR=$LIBICONV_LOC/include \
+        -DICONV_LIBRARIES=$LIBICONV_LOC/lib/libiconv.dylib \
+        -DJPEG_INCLUDE_DIR=$JPEG_LOC/include \
+        -DJPEG_LIBRARIES=$JPEG_LOC/lib/libjpeg.a \
+        -DGNUTLS_INCLUDE_DIR=$GNUTLS_LOC/include \
+        -DGNUTLS_LIBRARY=$GNUTLS_LOC/lib/libgnutls.dylib \
+        -DCMAKE_VERBOSE_MAKEFILE=OFF \
+    source &>/dev/null
+make install &>/dev/null
+
+###############################################################################
+# TigerVNC Server                                                             #
+###############################################################################
+
+## TODO: Server build fails; configuration needs help; see also https://www.xquartz.org/Developer-Info.html
+
+#mkdir unix
+#cp -R source/unix/xserver unix/
+#git clone git://github.com/XQuartz/xorg-server.git xorg-server # Change to download tarball
+#cd xorg-server && git checkout server-1.16-apple
+#cd ../ && cp -R xorg-server/* unix/xserver/
+#cp -R /opt/X11/include/xorg/* unix/xserver/
+#cd unix/xserver/
+#patch -p1 < ../../source/unix/xserver116.patch
+#export PKG_CONFIG_PATH="/opt/X11/lib/pkgconfig:/opt/X11/share/pkgconfig"
+#autoreconf -fiv -I/opt/X11/share/aclocal/ -I/opt/X11/include
+#
+#./configure --with-apple-application-name=XQuartz --with-pic --without-dtrace \
+#    --disable-static --disable-dri \
+#    --disable-xinerama --disable-xvfb --disable-xnest --disable-xorg \
+#    --disable-dmx --disable-xwin --disable-xephyr --disable-kdrive \
+#    --disable-config-dbus --disable-config-hal --disable-config-udev \
+#    --disable-dri2 --enable-install-libxf86config --enable-glx \
+#    --with-default-font-path="catalogue:/opt/X11/share/fonts/misc/,/opt/X11/share/fonts/TTF/,/opt/X11/share/fonts/OTF,/opt/X11/share/fonts/Type1/,/opt/X11/share/fonts/75dpi/:unscaled,/opt/X11/share/fonts/100dpi/:unscaled,/opt/X11/share/fonts/75dpi/,/opt/X11/share/fonts/100dpi/,/Library/Fonts,/System/Library/Fonts,built-ins" \
+#    --with-fontdir=/opt/X11/share/fonts \
+#    --with-xkb-path=/opt/X11/share/X11/xkb \
+#    --with-xkb-output=/opt/x11/var/cache/xkb \
+#    --with-xkb-bin-directory=/opt/X11/bin \
+#    --with-serverconfig-path=/opt/X11/lib/xorg \
+#    --with-dri-driver-path=/opt/X11/lib/dri \
+#
+#make TIGERVNC_SRCDIR="$ASSETS"/TigerVNC/source
+
+###############################################################################
+# Clean up.                                                                   #
+###############################################################################
+
+PATH="$ORIG_PATH"
 #cd $DIR
