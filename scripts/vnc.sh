1535dea3148e4fb86ca7d3d667a0bcd73dd867c7
Matthew Troncali <matt@troncali.net>
2018-04-29 14:43:32 -0700

    Add comments to Server section.

--- scripts/vnc.sh
+++ scripts/vnc.sh
@@ -22,7 +22,7 @@ TIGER_DIR="${SETUP_ASSETS}/TigerVNC/source"
 
 ORIG_PATH="$PATH"
 X11_LOC="opt/X11"
-BREWED_LOC="/usr/local/opt" # Assumes dependencies are installed by homebrew.
+BREWED_LOC="/usr/local/opt" # Assumes dependencies installed by homebrew.
 GETTEXT_LOC="$BREWED_LOC/gettext"
 LIBICONV_LOC="$BREWED_LOC/libiconv"
 JPEG_LOC="$BREWED_LOC/jpeg-turbo"
@@ -66,37 +66,43 @@ make install &>/dev/null
 # TigerVNC Server                                                             #
 ###############################################################################
 
-## TODO: Server build fails; configuration needs help; see also https://www.xquartz.org/Developer-Info.html
+# Copy source files to working directory.
+mkdir unix
+cp -R source/unix/xserver unix/
 
-#mkdir unix
-#cp -R source/unix/xserver unix/
-#git clone git://github.com/XQuartz/xorg-server.git xorg-server # Change to download tarball
-#cd xorg-server && git checkout server-1.16-apple
-#cd ../ && cp -R xorg-server/* unix/xserver/
-#cp -R /opt/X11/include/xorg/* unix/xserver/
-#cd unix/xserver/
-#patch -p1 < ../../source/unix/xserver116.patch
-#export PKG_CONFIG_PATH="/opt/X11/lib/pkgconfig:/opt/X11/share/pkgconfig"
-#autoreconf -fiv -I/opt/X11/share/aclocal/ -I/opt/X11/include
-#
-#./configure --with-apple-application-name=XQuartz --with-pic --without-dtrace \
-#    --disable-static --disable-dri \
-#    --disable-xinerama --disable-xvfb --disable-xnest --disable-xorg \
-#    --disable-dmx --disable-xwin --disable-xephyr --disable-kdrive \
-#    --disable-config-dbus --disable-config-hal --disable-config-udev \
-#    --disable-dri2 --enable-install-libxf86config --enable-glx \
-#    --with-default-font-path="catalogue:/opt/X11/share/fonts/misc/,/opt/X11/share/fonts/TTF/,/opt/X11/share/fonts/OTF,/opt/X11/share/fonts/Type1/,/opt/X11/share/fonts/75dpi/:unscaled,/opt/X11/share/fonts/100dpi/:unscaled,/opt/X11/share/fonts/75dpi/,/opt/X11/share/fonts/100dpi/,/Library/Fonts,/System/Library/Fonts,built-ins" \
-#    --with-fontdir=/opt/X11/share/fonts \
-#    --with-xkb-path=/opt/X11/share/X11/xkb \
-#    --with-xkb-output=/opt/x11/var/cache/xkb \
-#    --with-xkb-bin-directory=/opt/X11/bin \
-#    --with-serverconfig-path=/opt/X11/lib/xorg \
-#    --with-dri-driver-path=/opt/X11/lib/dri \
-#
-#make TIGERVNC_SRCDIR="$ASSETS"/TigerVNC/source
+# Get current stable version of Xserver for MacOS and copy to working dir.
+git clone git://github.com/XQuartz/xorg-server.git xorg-server
+cd xorg-server && git checkout server-1.16-apple
+cd ../ && cp -R xorg-server/* unix/xserver/
+    #System files unsuccessful: cp -R /opt/X11/include/xorg/* unix/xserver/
+
+# Patch Xserver for build.
+cd unix/xserver/
+patch -p1 < ../../source/unix/xserver116.patch
+
+## TODO: Server build fails; configuration needs help.
+##       See also https://www.xquartz.org/Developer-Info.html
+
+# Build.
+export PKG_CONFIG_PATH="/opt/X11/lib/pkgconfig:/opt/X11/share/pkgconfig"
+autoreconf -fiv -I/opt/X11/share/aclocal/ -I/opt/X11/include
+./configure --with-apple-application-name=XQuartz --with-pic --without-dtrace \
+    --disable-static --disable-dri \
+    --disable-xinerama --disable-xvfb --disable-xnest --disable-xorg \
+    --disable-dmx --disable-xwin --disable-xephyr --disable-kdrive \
+    --disable-config-dbus --disable-config-hal --disable-config-udev \
+    --disable-dri2 --enable-install-libxf86config --enable-glx \
+    --with-default-font-path="catalogue:/opt/X11/share/fonts/misc/,/opt/X11/share/fonts/TTF/,/opt/X11/share/fonts/OTF,/opt/X11/share/fonts/Type1/,/opt/X11/share/fonts/75dpi/:unscaled,/opt/X11/share/fonts/100dpi/:unscaled,/opt/X11/share/fonts/75dpi/,/opt/X11/share/fonts/100dpi/,/Library/Fonts,/System/Library/Fonts,built-ins" \
+    --with-fontdir=/opt/X11/share/fonts \
+    --with-xkb-path=/opt/X11/share/X11/xkb \
+    --with-xkb-output=/opt/x11/var/cache/xkb \
+    --with-xkb-bin-directory=/opt/X11/bin \
+    --with-serverconfig-path=/opt/X11/lib/xorg \
+    --with-dri-driver-path=/opt/X11/lib/dri \
+make TIGERVNC_SRCDIR="$TIGER_DIR"
 
 ###############################################################################
-# Clean up.                                                                   #
+# Clean up                                                                    #
 ###############################################################################
 
 PATH="$ORIG_PATH"
